efefa2b316c22ee8d4b9b499dc9c8c18de32613f
dattuanh <hdat1216@gmail.com>
2024-07-08 15:06:59 +0700

    fix product

--- Assignment_1 API/Controllers/ProductsController.cs
+++ Assignment_1 API/Controllers/ProductsController.cs
@@ -122,5 +122,28 @@ namespace Assignment_1_API.Controllers
         {
             return (_context.Products?.Any(e => e.ProductId == id)).GetValueOrDefault();
         }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<IEnumerable<Product>>> Search([FromQuery] string? pName, [FromQuery] decimal? unitPrice)
+        {
+            if (_context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var query = _context.Products.AsQueryable();
+
+            if (!string.IsNullOrEmpty(pName))
+            {
+                query = query.Where(p => p.ProductName.Contains(pName));
+            }
+
+            if (unitPrice.HasValue)
+            {
+                query = query.Where(p => p.UnitPrice == unitPrice.Value);
+            }
+
+            return await query.ToListAsync();
+        }
     }
 }
