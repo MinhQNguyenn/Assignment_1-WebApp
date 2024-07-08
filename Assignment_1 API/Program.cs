ecf0c87c523b601641cda061c781780000eca54f
dattuanh <hdat1216@gmail.com>
2024-07-08 13:45:21 +0700

    add program

--- Assignment_1 API/Program.cs
+++ Assignment_1 API/Program.cs
@@ -1,48 +1,22 @@
 using Assignment_1_API.Models;
-using Microsoft.AspNetCore.OData;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OData.Edm;
-using Microsoft.OData.ModelBuilder;
 
 namespace Assignment_1_API
 {
     public class Program
     {
-        private static IEdmModel GetEdmModel()
-        {
-            var builder = new ODataConventionModelBuilder();
-            builder.EntitySet<Category>("Categories");
-            builder.EntitySet<Product>("Products");
-            builder.EntitySet<Order>("Orders");
-            builder.EntitySet<OrderDetail>("OrderDetails");
-            builder.EntitySet<Staff>("Staffs");
-            return builder.GetEdmModel();
-        }
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            //var conf = new ConfigurationBuilder()
-            //    .AddJsonFile("appsettings.json")
-            //    .Build();
+
             // Add services to the container.
-            builder.Services.AddCors();
-            //ODataConventionModelBuilder modelBuilder = new ODataConventionModelBuilder();
-            //modelBuilder.EntitySet<Category>("Categories");
-            //modelBuilder.EntitySet<Product>("Products");
-            //modelBuilder.EntitySet<Staff>("Staffs");
-            //modelBuilder.EntitySet<Order>("Orders");
-            //modelBuilder.EntitySet<OrderDetail>("OrderDetails");
 
-            //confige Odata
-            builder.Services.AddControllers().AddOData(options => options.Select().Filter().Count().OrderBy().Expand().SetMaxTop(100).AddRouteComponents("odata", GetEdmModel()));
-
-            
-
-            //builder.Services.AddControllers();
+            builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddDbContext<MyStoreContext>();// (opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("MyStore")));
+            var connectionString = builder.Configuration.GetConnectionString("MyStoreDB");
+            builder.Services.AddDbContext<MyStoreContext>(options => { options.UseSqlServer(connectionString); });
 
             var app = builder.Build();
             app.UseCors(option => option.AllowAnyHeader().
@@ -63,7 +37,6 @@ namespace Assignment_1_API
             app.MapControllers();
 
             app.Run();
-
         }
     }
 }
